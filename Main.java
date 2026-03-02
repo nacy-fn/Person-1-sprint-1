faf1c2cd7c268e9bf9255a8251ff99b7269bb618
Nacyha FN <3173076f@student.gla.ac.uk>
2026-03-03 02:12:13 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,26 @@
+/**
+ * Entry point of the game application.
+ * Responsible for starting the game instance.
+ *
+ * @author 
+ */
 public class Main {
-    public static void main(String[] args) {
-        Game game = new Game();
-        game.startGame(); // US01 - Start Game
-        
-        // Demonstrate turn switching
-        game.endTurn(); // US12
-        game.endTurn(); // US12
-    }
+
+	/**
+	 * Application entry method.
+	 *
+	 * @param args command line arguments
+	 */
+	public static void main(String[] args) {
+
+		Game game = new Game();
+
+		// US01 - Start Game
+		game.startGame();
+
+		// US12 - Demonstrate turn switching
+		game.endTurn();
+		game.endTurn();
+	}
+
 }
